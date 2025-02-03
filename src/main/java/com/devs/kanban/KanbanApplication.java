acdfc018d84d39ceaa14c73c6f304210d69c6442
migue1angel <macf06200@gmail.com>
2025-02-03 09:11:41 -0500

    first commit

---
+++ src/main/java/com/devs/kanban/KanbanApplication.java
@@ -0,0 +1,14 @@
+package com.devs.kanban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        SpringApplication.run(KanbanApplication.class, args);
+    }
+
+}
